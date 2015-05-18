481d92328e9108bcbf23caa18c3fb91617a1ec69
Thiru Thirunavukarasu <thiru0130@gmail.com>
2015-05-17 21:43:36 -0400

    Added read macros from LOL

--- glu.lisp
+++ glu.lisp
@@ -491,3 +491,78 @@
     `(defmacro! ,name ,args
        `(let ,(mapcar #'list (list ,@gs) (list ,@os))
           ,(progn ,@body)))))
+
+;; Taken from Let Over Lambda (Daniel Herring's nestable suggestion)
+(defun sharp-double-quote-reader (stream sub-char numarg)
+  "Facilitates creating strings that have double-quote and backslashes without escaping."
+  (declare (ignore sub-char numarg))
+  (let (chars (state 'normal) (depth 1))
+    (loop do
+      (let ((curr (read-char stream)))
+        (cond ((eq state 'normal)
+                 (cond ((char= curr #\#)
+                          (push #\# chars)
+                          (setq state 'read-sharp))
+                       ((char= curr #\")
+                          (setq state 'read-quote))
+                       (t
+                          (push curr chars))))
+              ((eq state 'read-sharp)
+                 (cond ((char= curr #\")
+                          (push #\" chars)
+                          (incf depth)
+                          (setq state 'normal))
+                       (t
+                          (push curr chars)
+                          (setq state 'normal))))
+              ((eq state 'read-quote)
+                 (cond ((char= curr #\#)
+                          (decf depth)
+                          (if (zerop depth) (return))
+                          (push #\" chars)
+                          (push #\# chars)
+                          (setq state 'normal))
+                       (t
+                          (push #\" chars)
+                          (if (char= curr #\")
+                            (setq state 'read-quote)
+                            (progn
+                              (push curr chars)
+                              (setq state 'normal)))))))))
+   (coerce (nreverse chars) 'string)))
+(set-dispatch-macro-character
+  #\# #\" #'sharp-double-quote-reader)
+
+;; Take from Let Over Lambda (Martin Dirichs' version)
+(defun sharp-greater-than-reader (stream sub-char numarg)
+  "Facilitates creating strings with a user-defined terminator.
+   For example:
+     #>END
+     Any characters can be placed here. The only thing that will terminate the reading of this
+     string is...END"
+  (declare (ignore sub-char numarg))
+  (let (chars)
+    (do ((curr (read-char stream)
+               (read-char stream)))
+        ((char= #\newline curr))
+      (push curr chars))
+    (let ((pattern (nreverse chars))
+          output)
+      (labels ((match (pos chars)
+        (if (null chars)
+          pos
+          (if (char= (nth pos pattern) (car chars))
+              (match (1+ pos) (cdr chars))
+              (match 0 (cdr (append (subseq pattern 0 pos) chars)))))))
+        (do (curr
+             (pos 0))
+            ((= pos (length pattern)))
+          (setf curr (read-char stream)
+                pos (match pos (list curr)))
+          (push curr output))
+        (coerce
+          (nreverse
+            (nthcdr (length pattern) output))
+          'string)))))
+(set-dispatch-macro-character
+  #\# #\> #'sharp-greater-than-reader)
